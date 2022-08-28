215c08b97d4d5992d1199d24c465644598e415f1
bag33188 <bglatman@outlook.com>
2022-08-28 16:05:36 -0700

    fix

--- resources/views/rom-files/index.blade.php
+++ resources/views/rom-files/index.blade.php
@@ -7,11 +7,11 @@
 
             <button type="submit">download!</button>
         </form>
-        <!--<form method="POST" action="{{route('rom-files.destroy', ['romFile' => $romFile])}}">
+        <form method="POST" action="{{route('rom-files.destroy', ['romFile' => $romFile])}}">
             @method('DELETE')
             @csrf
 
             <button type="submit">delete!</button>
-        </form>-->
+        </form>
     @endforeach
 </x-app-layout>
