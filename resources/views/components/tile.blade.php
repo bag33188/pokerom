df5cdf8efb5dc9082b3f7a2329d7e31564a30724
bag33188 <bglatman@outlook.com>
2022-08-29 15:29:08 -0700

    fix

--- resources/views/components/tile.blade.php
+++ resources/views/components/tile.blade.php
@@ -1,5 +1,7 @@
-<div {{ $attributes->merge(['class'=> "border border-gray-200 bg-white shadow-md rounded w-full h-full
-    inline-grid grid-cols-1 grid-rows-[auto_auto] gap-y-2 p-2"]) }}>
+@php
+    $tileClasses = ['border', 'border-gray-200', 'bg-white', 'shadow-md', 'rounded', 'w-full', 'h-full', 'inline-grid', 'grid-cols-1', 'grid-rows-[auto_auto]', 'gap-y-2', 'p-2'];
+@endphp
+<div {{ $attributes->merge(['class'=> joinCssClasses($tileClasses)]) }}>
     <div class="p-2 border rounded-md shadow-inner border-gray-300 flex flex-col">
         {{$slot}}
     </div>
