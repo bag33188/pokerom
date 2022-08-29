c3b81a163d1142a00313ef18c201a591cef1e046
bag33188 <bglatman@outlook.com>
2022-08-28 18:42:35 -0700

    fix

--- resources/views/rom-files/index.blade.php
+++ resources/views/rom-files/index.blade.php
@@ -1,17 +1,20 @@
 <x-app-layout>
     @foreach($romFiles as $romFile)
-        <h3>{{$romFile->filename}}</h3>
-        <form method="GET" action="{{route('rom-files.download', ['romFile' => $romFile])}}">
-            @method('GET')
-            @csrf
+        <div class="py-3 px-3 border-2">
 
-            <x-jet-button type="submit">download!</x-jet-button>
-        </form>
-        <form method="POST" action="{{route('rom-files.destroy', ['romFile' => $romFile])}}">
-            @method('DELETE')
-            @csrf
+            <h3>{{$romFile->filename}}</h3>
+            <form method="GET" action="{{route('rom-files.download', ['romFile' => $romFile])}}">
+                @method('GET')
+                @csrf
 
-            <x-jet-danger-button type="submit">DELETE!</x-jet-danger-button>
-        </form>
+                <x-jet-button type="submit">download!</x-jet-button>
+            </form>
+            <form method="POST" action="{{route('rom-files.destroy', ['romFile' => $romFile])}}">
+                @method('DELETE')
+                @csrf
+
+                <x-jet-danger-button type="submit">DELETE!</x-jet-danger-button>
+            </form>
+        </div>
     @endforeach
 </x-app-layout>
