5109d64aabd2f442d74d2813d3ca164f83fac5a2
bag33188 <bglatman@outlook.com>
2022-08-29 10:37:47 -0700

    fix

--- app/Observers/UserObserver.php
+++ app/Observers/UserObserver.php
@@ -2,7 +2,19 @@
 
 namespace App\Observers;
 
+use App\Interfaces\UserRepositoryInterface;
+use App\Models\User;
+
 class UserObserver
 {
-    //
+    public function __construct(private readonly UserRepositoryInterface $userRepository)
+    {
+    }
+
+    public function updated(User $user): void
+    {
+        if (request()->is('api/*')) {
+            $this->userRepository->revokeApiTokens($user);
+        }
+    }
 }
