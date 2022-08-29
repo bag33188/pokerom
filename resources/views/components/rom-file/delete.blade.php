4bb27137e20af503fd9439d817a92117af02ed8c
bag33188 <bglatman@outlook.com>
2022-08-29 16:04:23 -0700

    fix

--- resources/views/components/rom-file/delete.blade.php
+++ resources/views/components/rom-file/delete.blade.php
@@ -1,7 +1,29 @@
+@once
+    @push('styles')
+        <style {!! 'type="text/css"' !!}>
+            .not-allowed {
+                cursor: not-allowed !important;
+            }
+        </style>
+    @endpush
+    @push('scripts')
+        <script type="text/javascript">
+            function disableDeleteBtn(deleteBtnId) {
+                const deleteBtn = document.getElementById(deleteBtnId);
+                deleteBtn.disabled = true;
+                deleteBtn.classList.add("not-allowed");
+            }
+        </script>
+    @endpush
+@endonce
+@php
+    $deleteBtnId = sprintf("delete-romFile-btn-%s", $romFile->id);
+@endphp
 <form {{ $attributes->merge(['class' => 'inline']) }} method="POST"
-      action="{{ route('rom-files.destroy', ['romFile' => $romFile]) }}">
+      action="{{ route('rom-files.destroy', ['romFile' => $romFile]) }}"
+      onsubmit="disableDeleteBtn('{{$deleteBtnId}}')">
     @method('DELETE')
     @csrf
 
-    <x-jet-danger-button type="submit">DELETE</x-jet-danger-button>
+    <x-jet-danger-button type="submit" id="{{ $deleteBtnId }}">DELETE</x-jet-danger-button>
 </form>
