ad2b162c5f4b15ac9d4eecb6c503256c0b81be67
bag33188 <bglatman@outlook.com>
2022-08-29 14:42:29 -0700

    fix

--- resources/views/vendor/jetstream/components/welcome.blade.php
+++ resources/views/vendor/jetstream/components/welcome.blade.php
@@ -1,6 +1,7 @@
 <div class="p-6 sm:px-20 bg-white border-b border-gray-200">
     <div>
-        <x-jet-application-logo class="block h-12 w-auto" />
+{{--        <x-jet-application-logo class="block h-12 w-auto" />--}}
+        @include('ui.app-logo')
     </div>
 
     <div class="mt-8 text-2xl">
