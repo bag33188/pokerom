7735053fc57533ea976185a669910fb771c2a701
bag33188 <bglatman@outlook.com>
2022-08-29 16:13:22 -0700

    fix

--- resources/views/components/rom-file/download.blade.php
+++ resources/views/components/rom-file/download.blade.php
@@ -1,8 +1,8 @@
 <form {{ $attributes->merge(['class' => 'inline']) }} method="GET"
       action="{{ route('rom-files.download', ['romFile' => $romFile]) }}"
-      name="download-rom-file-{{ $romFile->id }}{{-- -form --}}"
+      name="download-romFile-{{ $romFile->_id }}{{-- -form --}}"
       enctype="multipart/form-data">
     @method('GET')
     @csrf
-    <x-jet-button type="submit">Download</x-jet-button>
+    <x-jet-button type="submit" id="download-romFile-{{ $romFile->_id }}-btn">Download</x-jet-button>
 </form>
