b4df1b7b17bc62ed0c20d31ff7e9a9215a728293
bag33188 <bglatman@outlook.com>
2022-08-29 18:53:47 -0700

    fix

--- app/Http/Requests/StoreGameRequest.php
+++ app/Http/Requests/StoreGameRequest.php
@@ -32,7 +32,7 @@ class StoreGameRequest extends FormRequest
         $this->merge([
             'slug' => Str::slug($this->input('game_name')),
             'date_released' => Date::create($this->input('date_released'))->format('Y-m-d'),
-            'game_name' => preg_replace("/[\x{E9}\x{C9}]/u", 'e', $this->input('game_name'))
+            'game_name' => preg_replace("/[\x{E9}\x{C9}]/u", "\u{0065}", $this->input('game_name'))
         ]);
     }
 
