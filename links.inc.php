a8ea17a2dbe50d49fab0bbda7c72538681687850
Broccolini <50373500+bag33188@users.noreply.github.com>
2022-08-29 17:13:39 -0700

    add array of ancors

--- links.inc.php
+++ links.inc.php
@@ -5,3 +5,35 @@
 <li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-emerald-version/">Pokémon Emerald Version</a></li>
 <li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-platinum-version/">Pokémon Platinum Version</a></li>
 <li><a href="https://projectpokemon.org/home/tutorials/save-editing/using-pkhex/">Using PKHeX</a></li>
+<?php
+
+$anchors = [
+	[
+	'href'=>'https://www.pokemon.com/us/strategy/the-details-of-discovering-pokemon-eggs',
+	'text'=>'The Details of Discovering Pokémon Eggs'
+	],
+		[
+	'href'=>'https://www.pokemon.com/us/strategy/raising-battle-ready-pokemon/',
+	'text'=>'Raising Battle-Ready Pokémon!'
+	],
+			[
+	'href'=>'https://www.pokemon.com/us/pokemon-video-games/pokemon-ultra-sun-and-pokemon-ultra-moon/',
+	'text'=>'Pokémon Ultra Sun and Pokémon Ultra Moon'
+	],
+				[
+	'href'=>'https://www.pokemon.com/us/pokemon-video-games/all-pokemon-games/',
+	'text'=>'All Pokémon Video Games'
+	],
+	[
+	'href'=>'https://www.pokemon.com/us/pokemon-video-games/pokemon-emerald-version/',
+	'text'=>'Pokémon Emerald Version'
+	],
+		[
+	'href'=>'https://www.pokemon.com/us/pokemon-video-games/pokemon-platinum-version/',
+	'text'=>'Pokémon Platinum Version'
+	],
+		[
+	'href'=>'https://projectpokemon.org/home/tutorials/save-editing/using-pkhex/',
+	'text'=>'Using PKHeX'
+	],
+];?>
