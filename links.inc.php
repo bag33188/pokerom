c63cc6ba5ad132807686fcfa428bc0e9f06d3ece
bag33188 <bglatman@outlook.com>
2022-08-29 17:19:14 -0700

    fix

--- links.inc.php
+++ links.inc.php
@@ -1,3 +1,6 @@
+<?php
+echo /** @lang HTML */
+<<<'EOT'
 <!--<li><a href="https://www.pokemon.com/us/strategy/the-details-of-discovering-pokemon-eggs">The Details of Discovering Pokémon Eggs</a></li>-->
 <!--<li><a href="https://www.pokemon.com/us/strategy/raising-battle-ready-pokemon/">Raising Battle-Ready Pokémon!</a></li>-->
 <!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-ultra-sun-and-pokemon-ultra-moon/">Pokémon Ultra Sun and Pokémon Ultra Moon</a></li>-->
@@ -5,7 +8,8 @@
 <!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-emerald-version/">Pokémon Emerald Version</a></li>-->
 <!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-platinum-version/">Pokémon Platinum Version</a></li>-->
 <!--<li><a href="https://projectpokemon.org/home/tutorials/save-editing/using-pkhex/">Using PKHeX</a></li>-->
-<?php
+EOT;
+
 $anchors = array(
     [
         'href' => 'https://www.pokemon.com/us/strategy/the-details-of-discovering-pokemon-eggs',
@@ -38,5 +42,5 @@ $anchors = array(
 );
 ?>
 <?php foreach ($anchors as $anchor): ?>
-    <li><a href="<?php echo $anchor['href']; ?>"><?php echo $anchor['text']; ?></a></li>
+    <li><a href="<?= $anchor['href']; ?>"><?= $anchor['text']; ?></a></li>
 <?php endforeach; ?>
