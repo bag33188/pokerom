290c0ac004257758fc19364c8c64bbeca5b261ba
bag33188 <bglatman@outlook.com>
2022-08-29 18:23:38 -0700

    fix

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -20,7 +20,7 @@
                 <x-list-item>{{ $game->region }} Region</x-list-item>
                 <x-list-item>{{ $formatGameType($game->game_type) }}</x-list-item>
                 <x-list-item style="border-bottom: 0 !important;">Released
-                    on {{ $game->date_released->format('l, F jS, Y') }}</x-list-item>
+                    on: {{ $game->date_released->format('l, F jS, Y') }}</x-list-item>
             </x-list-group>
             @if($userIsAdmin)
                 <div class="row-start-2 row-end-2 ml-1 col-start-2 col-end-2 justify-self-end">
