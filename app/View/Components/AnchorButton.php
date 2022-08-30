f14d29bf407c726c6bdf0f890309fa161748e1f9
bag33188 <bglatman@outlook.com>
2022-08-29 18:49:06 -0700

    fix

--- app/View/Components/AnchorButton.php
+++ app/View/Components/AnchorButton.php
@@ -29,13 +29,13 @@ class AnchorButton extends Component
      */
     public function render(): View|string|Closure
     {
-        $anchorPrimaryClasses = ['inline-flex', 'items-center', 'px-4', 'py-2', 'bg-gray-800', 'border', 'border-transparent', 'rounded-md', 'font-semibold', 'text-xs', 'text-white', 'uppercase', 'tracking-widest', 'hover:bg-gray-700', 'active:bg-gray-900', 'focus:outline-none', 'focus:border-gray-900', 'focus:ring', 'focus:ring-gray-300', 'disabled:opacity-25', 'transition'];
-        $anchorSecondaryClasses = ['inline-flex', 'items-center', 'px-4', 'py-2', 'bg-white', 'border', 'border-gray-300', 'rounded-md', 'font-semibold', 'text-xs', 'text-gray-700', 'uppercase', 'tracking-widest', 'shadow-sm', 'hover:text-gray-500', 'focus:outline-none', 'focus:border-blue-300', 'focus:ring', 'focus:ring-blue-200', 'active:text-gray-800', 'active:bg-gray-50', 'disabled:opacity-25', 'transition'];
-        $anchorDangerClasses = ['inline-flex', 'items-center', 'px-4', 'py-2', 'bg-gray-800', 'border', 'border-transparent', 'rounded-md', 'font-semibold', 'text-xs', 'text-white', 'uppercase', 'tracking-widest', 'hover:bg-gray-700', 'active:bg-gray-900', 'focus:outline-none', 'focus:border-gray-900', 'focus:ring', 'focus:ring-gray-300', 'disabled:opacity-25', 'transition'];
+        $anchorPrimary = ['inline-flex', 'items-center', 'px-4', 'py-2', 'bg-gray-800', 'border', 'border-transparent', 'rounded-md', 'font-semibold', 'text-xs', 'text-white', 'uppercase', 'tracking-widest', 'hover:bg-gray-700', 'active:bg-gray-900', 'focus:outline-none', 'focus:border-gray-900', 'focus:ring', 'focus:ring-gray-300', 'disabled:opacity-25', 'transition'];
+        $anchorSecondary = ['inline-flex', 'items-center', 'px-4', 'py-2', 'bg-white', 'border', 'border-gray-300', 'rounded-md', 'font-semibold', 'text-xs', 'text-gray-700', 'uppercase', 'tracking-widest', 'shadow-sm', 'hover:text-gray-500', 'focus:outline-none', 'focus:border-blue-300', 'focus:ring', 'focus:ring-blue-200', 'active:text-gray-800', 'active:bg-gray-50', 'disabled:opacity-25', 'transition'];
+        $anchorDanger = ['inline-flex', 'items-center', 'px-4', 'py-2', 'bg-gray-800', 'border', 'border-transparent', 'rounded-md', 'font-semibold', 'text-xs', 'text-white', 'uppercase', 'tracking-widest', 'hover:bg-gray-700', 'active:bg-gray-900', 'focus:outline-none', 'focus:border-gray-900', 'focus:ring', 'focus:ring-gray-300', 'disabled:opacity-25', 'transition'];
         return view('components.anchor-button', [
-            'anchorPrimary' => $anchorPrimaryClasses,
-            'anchorSecondary' => $anchorSecondaryClasses,
-            'anchorDanger' => $anchorDangerClasses,
+            'anchorPrimaryClasses' => $anchorPrimary,
+            'anchorSecondaryClasses' => $anchorSecondary,
+            'anchorDangerClasses' => $anchorDanger,
         ]);
     }
 }
