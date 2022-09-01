2b2fe5b7c5ff2483fc2faa99169dab5c50780831
bag33188 <bglatman@outlook.com>
2022-09-01 15:00:10 -0700

    fix

--- resources/views/roms/show.blade.php
+++ resources/views/roms/show.blade.php
@@ -6,17 +6,21 @@
     </x-slot>
     <div class="py-6 px-5">
         <x-list-group>
-            <x-list-item>ROM ID: {{ $rom->id }}</x-list-item>
-            <x-list-item>ROM Name: {{ $rom->rom_name }}</x-list-item>
-            <x-list-item>ROM Size: {{ $romQueries->formatRomSizeSQL($rom->rom_size) }}</x-list-item>
-            <x-list-item>ROM Type: {{ $rom->rom_type }}</x-list-item>
+            <x-list-item><p class="mt-1.5 inline-block">ROM Info</p>
+                <x-list-group class="border-0 py-2">
+                    <x-list-item>ROM ID: {{ $rom->id }}</x-list-item>
+                    <x-list-item>ROM Name: {{ $rom->rom_name }}</x-list-item>
+                    <x-list-item>ROM Size: {{ $romQueries->formatRomSizeSQL($rom->rom_size) }}</x-list-item>
+                    <x-list-item class="!border-b-0">ROM Type: {{ $rom->rom_type }}</x-list-item>
+                </x-list-group>
+            </x-list-item>
             @if($rom->has_game)
                 <x-list-item><p class="mt-1.5 inline-block">Game Info</p>
                     <x-list-group class="border-0 py-2">
                         <x-list-item class="border-0">Game ID: {{ $rom->game->id }}</x-list-item>
                         <x-list-item>Game Name: {{ $rom->game->game_name }}</x-list-item>
                         <x-list-item>Region: {{ $rom->game->region }}</x-list-item>
-                        <x-list-item class="border-0">Release
+                        <x-list-item class="!border-b-0">Release
                             Date: {{ $rom->game->date_released->format('l, F jS, Y') }}</x-list-item>
                     </x-list-group>
                 </x-list-item>
@@ -29,7 +33,7 @@
                         <x-list-item>Designated
                             Console: {{ $romFileRepository->determineConsole($rom->romFile) }}</x-list-item>
                         <x-list-item>File Length: {{ $rom->romFile->length }} Bytes</x-list-item>
-                        <x-list-item class="border-0">MD5 Hash: {{ $rom->romFile->md5 }}</x-list-item>
+                        <x-list-item class="!border-b-0">MD5 Hash: {{ $rom->romFile->md5 }}</x-list-item>
                     </x-list-group>
                 </x-list-item>
             @endif
