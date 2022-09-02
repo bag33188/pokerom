b40463370f232f39c24c266c947b22313ed95ad2
Broccolini <bglatman@callutheran.edu>
2022-09-02 01:48:04 -0700

    fix

--- resources/views/components/tile.blade.php
+++ resources/views/components/tile.blade.php
@@ -1,7 +1,8 @@
 @php
     $tileClasses = [
         'border', 'border-gray-200', 'bg-white', 'shadow-md', 'rounded', 'w-full',
-        'h-full', 'inline-grid', 'grid-cols-1', 'grid-rows-[auto_auto]', 'gap-y-2', 'p-2'
+        'h-full', 'inline-grid', 'grid-cols-1', 'grid-rows-[auto_auto]', 'gap-y-2',
+        'p-2'
     ];
 @endphp
 <div {{ $attributes->merge(['class'=> joinCssClasses($tileClasses)]) }}>
