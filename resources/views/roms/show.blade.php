b801780dd8e75c14ea148dc94d3ed8be23911e5f
bag33188 <bglatman@outlook.com>
2022-09-01 19:02:00 -0700

    fix

--- resources/views/roms/show.blade.php
+++ resources/views/roms/show.blade.php
@@ -5,9 +5,10 @@
         <h2 class="text-2xl text-center font-semibold leading-tight text-gray-900">{{ $rom->rom_name }} Information</h2>
     </x-slot>
     <div class="py-6 px-5">
-        <x-list-group class="shadow">
-            <x-list-item class="pb-4"><p class="mt-1.5 mb-3 inline-block font-semibold">ROM Info</p>
-                <x-list-group class="!border-1 rounded-lg -border-gray-200 bg-gray-100">
+        <x-list-group class="shadow" x-data="{ open1: true, open2: true, open3: true }">
+            <x-list-item class="pb-4"><p class="mt-1.5 mb-3 inline-block font-semibold" @click="open1 = !open1">ROM
+                    Info</p>
+                <x-list-group class="!border-1 rounded-lg -border-gray-200 bg-gray-100" x-show="open1">
                     <x-list-item>ROM ID: {{ $rom->id }}</x-list-item>
                     <x-list-item>ROM Name: {{ $rom->rom_name }}</x-list-item>
                     <x-list-item>ROM Size: {{ $romQueries->formatRomSizeSQL($rom->rom_size) }}</x-list-item>
@@ -15,8 +16,9 @@
                 </x-list-group>
             </x-list-item>
             @if($rom->has_game)
-                <x-list-item class="pb-4"><p class="mt-1.5 mb-3 inline-block font-semibold">Game Info</p>
-                    <x-list-group class="!border-1 rounded-lg -border-gray-200 bg-gray-100">
+                <x-list-item class="pb-4"><p class="mt-1.5 mb-3 inline-block font-semibold" @click="open2 = !open2">Game
+                        Info</p>
+                    <x-list-group class="!border-1 rounded-lg -border-gray-200 bg-gray-100" x-show="open2">
                         <x-list-item>Game ID: {{ $rom->game->id }}</x-list-item>
                         <x-list-item>Game Name: {{ $rom->game->game_name }} Version</x-list-item>
                         <x-list-item>Region: {{ $rom->game->region }}</x-list-item>
@@ -27,8 +29,9 @@
                 </x-list-item>
             @endif
             @if($rom->has_file)
-                <x-list-item class="pb-4"><p class="mt-1.5 mb-3 inline-block font-semibold">File Info</p>
-                    <x-list-group class="!border-1 rounded-lg -border-gray-200 bg-gray-100">
+                <x-list-item class="pb-4"><p class="mt-1.5 mb-3 inline-block font-semibold" @click="open3 = !open3">File
+                        Info</p>
+                    <x-list-group class="!border-1 rounded-lg -border-gray-200 bg-gray-100" x-show="open3">
                         <x-list-item>File ID: {{ $rom->romFile->_id }}</x-list-item>
                         <x-list-item>File Name: {{ $rom->romFile->filename }}</x-list-item>
                         <x-list-item>File Length: {{ $rom->romFile->length }} Bytes</x-list-item>
