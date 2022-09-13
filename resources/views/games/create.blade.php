ea204e83f2d2a4c77365373a17252f7ab55b26cc
bag33188 <bglatman@outlook.com>
2022-09-13 16:58:16 -0700

    fix

--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -3,20 +3,20 @@
         <h2 class="text-2xl text-center font-semibold leading-tight text-gray-900">Add Game</h2>
     </x-slot>
     <div class="container mx-auto w-full mt-3.5">
-        @if(count($romsWithNoGame) > 0)
-            <x-jet-validation-errors class="mb-4" />
+        @if($romsWithNoGame->count() > 0)
+            <x-jet-validation-errors class="mb-4"/>
             <form method="POST" action="{{ route('games.store') }}">
                 @method('POST')
                 @csrf
 
-                <x-form-select-label text="Assoc. ROM" for="availableRoms" />
+                <x-form-select-label text="Assoc. ROM" for="availableRoms"/>
                 <x-form-select id="availableRoms" name="rom_id" autofocus required>
                     @foreach($romsWithNoGame as $rom)
                         <option value="{{ $rom->id }}">{{ $rom->rom_name }}</option>
                     @endforeach
                 </x-form-select>
                 <div class="mt-2.5">
-                    <x-jet-label for="gameName" :value="__('Game Name')" />
+                    <x-jet-label for="gameName" :value="__('Game Name')"/>
                     <x-jet-input id="gameName" class="block mt-1 w-full" type="text"
                                  name="game_name"
                                  minlength="{{ MIN_GAME_NAME_LENGTH }}"
@@ -25,7 +25,7 @@
                     />
                 </div>
                 <div class="mt-2.5">
-                    <x-jet-label for="gameType" :value="__('Game Type')" />
+                    <x-jet-label for="gameType" :value="__('Game Type')"/>
                     <x-form-select name="game_type" id="gameType" required autofocus>
                         <option value="" selected>Select Game Type</option>
                         @foreach(GAME_TYPES as $index => $gameType)
@@ -35,7 +35,7 @@
                     </x-form-select>
                 </div>
                 <div class="mt-2.5">
-                    <x-jet-label for="gameRegion" :value="__('Region')" />
+                    <x-jet-label for="gameRegion" :value="__('Region')"/>
                     <x-form-select
                         name="region" id="gameRegion"
                         required autofocus>
@@ -46,17 +46,17 @@
                     </x-form-select>
                 </div>
                 <div class="mt-2.5">
-                    <x-jet-label for="dateReleased" :value="__('Date Released')" />
+                    <x-jet-label for="dateReleased" :value="__('Date Released')"/>
                     <x-jet-input type="date"
                                  class="block mt-1 w-full"
-                                 id="dateReleased" name="date_released" required autofocus />
+                                 id="dateReleased" name="date_released" required autofocus/>
                 </div>
                 <div class="mt-2.5">
-                    <x-jet-label for="generation" :value="__('Generation')" />
+                    <x-jet-label for="generation" :value="__('Generation')"/>
                     <x-jet-input type="number" id="generation" name="generation"
                                  class="block mt-1 w-full"
                                  min="{{ MIN_GAME_GENERATION_VALUE }}" max="{{ MAX_GAME_GENERATION_VALUE }}" required
-                                 autofocus />
+                                 autofocus/>
                 </div>
                 <div class="mt-4 w-full inline-flex flex-row justify-end">
                     <x-jet-button type="submit">
