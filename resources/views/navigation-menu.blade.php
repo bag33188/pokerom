05f08fc56da9428de99a5a8577f7aae44542ad95
bag33188 <bglatman@outlook.com>
2022-09-13 23:01:33 -0700

    fix

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -51,7 +51,7 @@
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
-                <!-- Teams Dropdown -->
+                {{--<!-- Teams Dropdown -->--}}
                 @if (Laravel\Jetstream\Jetstream::hasTeamFeatures())
                     <div class="ml-3 relative">
                         <x-jet-dropdown align="right" width="60">
