c3f669a81c5f4d52319685d067115398da9c0479
bag33188 <bglatman@outlook.com>
2022-09-13 22:41:45 -0700

    fix

--- links.inc.php
+++ links.inc.php
@@ -1,16 +1,14 @@
 <?php
-$old_anchors_str = /** @lang HTML */
-<<<'html'
-<!--<li><a href="https://www.pokemon.com/us/strategy/the-details-of-discovering-pokemon-eggs">The Details of Discovering Pokémon Eggs</a></li>-->
-<!--<li><a href="https://www.pokemon.com/us/strategy/raising-battle-ready-pokemon/">Raising Battle-Ready Pokémon!</a></li>-->
-<!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-ultra-sun-and-pokemon-ultra-moon/">Pokémon Ultra Sun and Pokémon Ultra Moon</a></li>-->
-<!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/all-pokemon-games/">All Pokémon Video Games</a></li>-->
-<!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-emerald-version/">Pokémon Emerald Version</a></li>-->
-<!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-platinum-version/">Pokémon Platinum Version</a></li>-->
-<!--<li><a href="https://projectpokemon.org/home/tutorials/save-editing/using-pkhex/">Using PKHeX</a></li>-->
-html;
-
-# print $old_anchors_str;
+# echo /** @lang HTML */
+# <<<'html'
+# <!--<li><a href="https://www.pokemon.com/us/strategy/the-details-of-discovering-pokemon-eggs">The Details of Discovering Pokémon Eggs</a></li>-->
+# <!--<li><a href="https://www.pokemon.com/us/strategy/raising-battle-ready-pokemon/">Raising Battle-Ready Pokémon!</a></li>-->
+# <!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-ultra-sun-and-pokemon-ultra-moon/">Pokémon Ultra Sun and Pokémon Ultra Moon</a></li>-->
+# <!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/all-pokemon-games/">All Pokémon Video Games</a></li>-->
+# <!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-emerald-version/">Pokémon Emerald Version</a></li>-->
+# <!--<li><a href="https://www.pokemon.com/us/pokemon-video-games/pokemon-platinum-version/">Pokémon Platinum Version</a></li>-->
+# <!--<li><a href="https://projectpokemon.org/home/tutorials/save-editing/using-pkhex/">Using PKHeX</a></li>-->
+# html;
 
 $anchors = array(
     [
