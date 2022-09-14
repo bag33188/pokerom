03d7a9d943f1d910d691516d27f35f4492d0786a
bag33188 <bglatman@outlook.com>
2022-09-14 01:19:48 -0700

    fix

---
+++ scripts/cmd/gen_pw.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+# ========================
+# Generate Password Script
+# ========================
+
+:<< --MULTILINE-COMMENT--
+Permissions (Unix)
+------------------
+
+add to current permissions
+$ cd scripts/cmd
+$ chmod +x ./gen_pw.sh
+
+set current permissions
+$ cd scripts/cmd
+$ chmod 755 ./gen_pw.sh
+--MULTILINE-COMMENT--
+
+gen_pw() {
+  export NODE_ENV=development
+  target_PWD=$(readlink -f .)
+  current_folder="${target_PWD##*/}"
+    if [[ "$current_folder" = "scripts" ]]; then
+        # echo 'im in scripts'
+        cd ..
+    fi
+    if [[ "$current_folder" = "cmd" ]]; then
+        # echo 'im in cmd'
+        cd ../..
+    fi
+  pw_gen_script_location="./scripts/helpers/password-hasher.js"
+  salt_val=$1
+  node $pw_gen_script_location --salt="$salt_val"
+}
+
+gen_pw "$@"
+
+exit 0;
