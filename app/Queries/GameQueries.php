a37116174ead5cbed315b2d51bff02c0f8105f93
bag33188 <bglatman@outlook.com>
2022-09-20 21:07:14 -0700

    update code base

--- app/Queries/GameQueries.php
+++ app/Queries/GameQueries.php
@@ -38,7 +38,10 @@ class GameQueries implements GameQueriesInterface
         $sql = /** @lang MariaDB */
             "SELECT * FROM `games` WHERE `game_type` = :rom_hack OR `generation` = 0 ORDER BY `date_released` DESC LIMIT :limit_results_to;";
         $query = DB::raw($sql);
-        return $this->game->fromQuery($query, ['rom_hack' => GAME_TYPES[self::findRomHackIndexOfGameTypes()], 'limit_results_to' => 10]);
+        return $this->game->fromQuery($query, [
+            'rom_hack' => GAME_TYPES[self::findRomHackIndexOfGameTypes()],
+            'limit_results_to' => 10
+        ]);
     }
 
     private static function findRomHackIndexOfGameTypes(): int
