d26964346f40e4203deb5f4e6376326fa8f294d5
bag33188 <bglatman@outlook.com>
2022-09-21 12:32:56 -0700

    update code base

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -15,18 +15,18 @@
     ];
 
     $conditionalAlertCssClasses = [
-        'bg-green-100' => $alertType === AlertType::SUCCESS,
-        'border-green-400' => $alertType === AlertType::SUCCESS,
-        'text-green-700' => $alertType === AlertType::SUCCESS,
-        'bg-red-100' => $alertType === AlertType::ERROR,
-        'border-red-400' => $alertType === AlertType::ERROR,
-        'text-red-700' => $alertType === AlertType::ERROR,
-        'bg-yellow-100' => $alertType === AlertType::WARNING,
-        'border-yellow-400' => $alertType === AlertType::WARNING,
-        'text-yellow-700' => $alertType === AlertType::WARNING,
-        'bg-blue-100' => $alertType === AlertType::DEFAULT,
-        'border-blue-400' => $alertType === AlertType::DEFAULT,
-        'text-blue-700' => $alertType === AlertType::DEFAULT,
+        'bg-green-100' => $alertType == AlertType::SUCCESS,
+        'border-green-400' => $alertType == AlertType::SUCCESS,
+        'text-green-700' => $alertType == AlertType::SUCCESS,
+        'bg-red-100' => $alertType == AlertType::ERROR,
+        'border-red-400' => $alertType == AlertType::ERROR,
+        'text-red-700' => $alertType == AlertType::ERROR,
+        'bg-yellow-100' => $alertType == AlertType::WARNING,
+        'border-yellow-400' => $alertType == AlertType::WARNING,
+        'text-yellow-700' => $alertType == AlertType::WARNING,
+        'bg-blue-100' => $alertType == AlertType::DEFAULT,
+        'border-blue-400' => $alertType == AlertType::DEFAULT,
+        'text-blue-700' => $alertType == AlertType::DEFAULT,
     ];
 @endphp
 <div @class(array_merge($universalAlertCssClasses, $conditionalAlertCssClasses))
