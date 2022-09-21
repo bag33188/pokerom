cfb078da0a7a0ecabf4dda6ab2461eb15a12bd0a
bag33188 <bglatman@outlook.com>
2022-09-21 16:32:40 -0700

    update code base

--- resources/views/rom-files/show.blade.php
+++ resources/views/rom-files/show.blade.php
@@ -24,24 +24,21 @@
                             datetime="{{ $romFileRepository->formatUploadDate($romFile->uploadDate, DATE_W3C, 'GMT') }}">{{ $romFileRepository->formatUploadDate($romFile->uploadDate, 'm-d-Y, h:i:s A (T, I)', 'PST8PDT') }}</time>
                     </x-list-item>
                     <x-list-item>Content Hash (MD5):
-                        <data value="{{ strtoupper($romFile->md5) }}">{{ $romFile->md5 }}</data>
+                        <data value="{{ strtoupper($romFile->md5) }}"><code>{{ $romFile->md5 }}</code></data>
                     </x-list-item>
                 </x-list-group>
             </div>
             <div class="order-1 mt-2.5 inline-flex flex-row justify-between">
-                @if($userIsAdmin)
-                    <div class="inline-block order-1">
-
+                <div class="inline-block order-1">
+                    @if($userIsAdmin)
                         <x-rom-file.delete class="order-1" :romFile="$romFile"/>
-                    </div>
-                @else
-                    <div class="inline-block order-1">
+                    @else
                         <x-anchor-button :btn-type="\App\Enums\AnchorButtonTypeEnum::SECONDARY"
                                          :href="route('rom-files.index')">
                             Go Back!
                         </x-anchor-button>
-                    </div>
-                @endif
+                    @endif
+                </div>
                 <div class="inline-block order-0">
                     <x-rom-file.download :romFile="$romFile"/>
                 </div>
