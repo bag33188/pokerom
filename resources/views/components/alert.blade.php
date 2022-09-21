de1ed77468177f11aa2ece568e8fffa1e453e87a
bag33188 <bglatman@outlook.com>
2022-09-21 16:08:34 -0700

    update code base

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -5,69 +5,50 @@
     $universalAlertCssClasses = [
         'my-6',
         'mx-4',
-        'border',
         'px-4',
         'py-3',
+        'border',
         'rounded',
         'relative',
-        'text-center',
+        'text-center'
     ];
-    $conditionalAlertCssClasses = [];
-//
-//    $conditionalAlertCssClasses = [
-//        'bg-green-100' => $alertType == AlertType::SUCCESS,
-//        'border-green-400' => $alertType == AlertType::SUCCESS,
-//        'text-green-700' => $alertType == AlertType::SUCCESS,
-//        'bg-red-100' => $alertType == AlertType::ERROR,
-//        'border-red-400' => $alertType == AlertType::ERROR,
-//        'text-red-700' => $alertType == AlertType::ERROR,
-//        'bg-yellow-100' => $alertType == AlertType::WARNING,
-//        'border-yellow-400' => $alertType == AlertType::WARNING,
-//        'text-yellow-700' => $alertType == AlertType::WARNING,
-//        'bg-blue-100' => $alertType == AlertType::DEFAULT,
-//        'border-blue-400' => $alertType == AlertType::DEFAULT,
-//        'text-blue-700' => $alertType == AlertType::DEFAULT,
-//    ];
-//    $conditionalAlertCssClasses1 = [
-//        'bg-green-100 border-green-400 text-green-700' => $alertType == AlertType::SUCCESS,
-//        'bg-red-100 border-red-400 text-red-700' => $alertType == AlertType::ERROR,
-//        'bg-yellow-100 border-yellow-400 text-yellow-700' => $alertType == AlertType::WARNING,
-//        'bg-blue-100 border-blue-400 text-blue-700' => $alertType == AlertType::DEFAULT,
-//    ];
+
+    $conditionalAlertCssClasses = array();
+
     switch ($alertType) {
         case AlertType::SUCCESS:
             $conditionalAlertCssClasses = [
                 'bg-green-100',
-                'border-green-400',
                 'text-green-700',
+                'border-green-400',
             ];
             break;
         case AlertType::ERROR:
             $conditionalAlertCssClasses = [
                 'bg-red-100',
-                'border-red-400',
                 'text-red-700',
+                'border-red-400',
             ];
             break;
         case AlertType::WARNING:
             $conditionalAlertCssClasses = [
                 'bg-yellow-100',
-                'border-yellow-400',
                 'text-yellow-700',
+                'border-yellow-400',
             ];
             break;
         case AlertType::INFO:
             $conditionalAlertCssClasses = [
                 'bg-blue-100',
-                'border-blue-400',
                 'text-blue-700',
+                'border-blue-400',
             ];
             break;
         default:
             $conditionalAlertCssClasses = [
                 'bg-gray-100',
-                'border-gray-500',
                 'text-gray-600',
+                'border-gray-500',
             ];
     }
 @endphp
--- resources/views/components/anchor-button.blade.php
+++ resources/views/components/anchor-button.blade.php
@@ -2,38 +2,37 @@
     use App\Enums\AnchorButtonTypeEnum as AnchorBtnType;
 
     $universalAnchorBtnCssClasses = [
-        'focus:outline-none',
-        'disabled:opacity-25',
-        'px-4',
-        'py-2',
-        'border',
-        'text-xs',
-        'uppercase',
-        'rounded-md',
-        'focus:ring',
-        'transition',
-        'inline-flex',
-        'items-center',
-        'font-semibold',
-        'tracking-widest',
-        'focus:outline-none',
-        'disabled:opacity-25',
+        "px-4",
+        "py-2",
+        "border",
+        "text-xs",
+        "uppercase",
+        "shadow-sm",
+        "rounded-md",
+        "focus:ring",
+        "transition",
+        "inline-flex",
+        "items-center",
+        "font-semibold",
+        "tracking-widest",
+        "focus:outline-none",
+        "disabled:opacity-25"
     ];
 
     $conditionalAnchorBtnClasses = [
         'text-white' => match ($btnType) {
-            AnchorBtnType::DANGER, AnchorBtnType::PRIMARY, AnchorBtnType::INFO => true,
+            AnchorBtnType::PRIMARY, AnchorBtnType::INFO, AnchorBtnType::DANGER => true,
             default => false
         },
         'border-transparent' => match ($btnType) {
-            AnchorBtnType::DANGER, AnchorBtnType::PRIMARY, AnchorBtnType::CAUTION => true,
+            AnchorBtnType::PRIMARY, AnchorBtnType::CAUTION, AnchorBtnType::DANGER => true,
             default => false
         },
+        'bg-gray-800 hover:bg-gray-700 active:bg-gray-900 focus:ring-gray-300 focus:border-gray-900' => $btnType == AnchorBtnType::PRIMARY,
+        'bg-white text-gray-700 border-gray-300 active:bg-gray-50 hover:text-gray-500 focus:ring-blue-200 active:text-gray-800 focus:border-blue-300' => $btnType == AnchorBtnType::SECONDARY,
+        'bg-blue-800 hover:bg-blue-700 active:bg-blue-900 focus:ring-blue-300 focus:border-blue-900' => $btnType == AnchorBtnType::INFO,
+        'bg-yellow-400 hover:bg-yellow-500 active:bg-yellow-600 focus:ring-yellow-300 focus:border-yellow-700' => $btnType == AnchorBtnType::CAUTION,
         'bg-red-600 hover:bg-red-500 active:bg-red-600 focus:ring-red-200 focus:border-red-700' => $btnType == AnchorBtnType::DANGER,
-        'bg-white shadow-sm text-gray-700 border-gray-300 active:bg-gray-50 hover:text-gray-500 focus:ring-blue-200 active:text-gray-800 focus:border-blue-300' => $btnType == AnchorBtnType::SECONDARY,
-        'hover:bg-gray-700 active:bg-gray-900 focus:ring-gray-300 focus:border-gray-900 bg-gray-800' => $btnType == AnchorBtnType::PRIMARY,
-        'hover:bg-blue-700 active:bg-blue-900 focus:ring-blue-300 focus:border-blue-900 bg-blue-800' => $btnType == AnchorBtnType::INFO,
-        'hover:bg-yellow-500 active:bg-yellow-600 focus:ring-yellow-300 focus:border-yellow-700 bg-yellow-400' => $btnType == AnchorBtnType::CAUTION,
     ];
 @endphp
 <a {{ $attributes }}
