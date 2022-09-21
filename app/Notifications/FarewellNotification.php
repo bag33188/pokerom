153074388d797a7a51f5ddc5fe3539a1c6ca8af1
bag33188 <bglatman@outlook.com>
2022-09-21 00:58:51 -0700

    update code base

--- app/Notifications/FarewellNotification.php
+++ app/Notifications/FarewellNotification.php
@@ -7,6 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Config;
+use JetBrains\PhpStorm\ArrayShape;
 
 class FarewellNotification extends Notification
 {
@@ -15,7 +16,11 @@ class FarewellNotification extends Notification
     public User $user;
 
     private static string $appName;
+
     private readonly string $farewellMessage;
+    private readonly string $subject;
+    private readonly string $salutation;
+    #[ArrayShape(['address' => "string", 'name' => "string"])] private readonly array $from;
 
     /**
      * Create a new notification instance.
@@ -25,14 +30,17 @@ class FarewellNotification extends Notification
     public function __construct(User $user)
     {
         $this->user = $user;
+        self::$appName = preg_replace("/^Poke/", POKE_EACUTE, ucfirst(Config::get('app.name')));
 
         $this->setMailProps();
     }
 
     private function setMailProps(): void
     {
-        self::$appName = preg_replace("/^Poke/", POKE_EACUTE, ucfirst(Config::get('app.name')));
         $this->farewellMessage = "{$this->user->name}, we're sad to see you leave.";
+        $this->from = ['address' => config('mail.from.address'), 'name' => self::$appName];
+        $this->salutation = sprintf("Thank you for using %s!", self::$appName);
+        $this->subject = "I guess it's goodbye for now...";
     }
 
     /**
@@ -55,10 +63,10 @@ class FarewellNotification extends Notification
     public function toMail(mixed $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->subject("I guess it's goodbye for now...")
-            ->from(config('mail.from.address'), self::$appName)
+            ->subject($this->subject)
+            ->from(...array_values($this->from))
             ->line($this->farewellMessage)
-            ->line(sprintf("Thank you for using %s!", self::$appName));
+            ->line($this->salutation);
     }
 
     /**
@@ -70,10 +78,10 @@ class FarewellNotification extends Notification
     public function toArray(mixed $notifiable): array
     {
         return [
-            'subject' => "I guess it's goodbye for now...",
-            'from' => [config('mail.from.address'), self::$appName],
+            'subject' => $this->subject,
+            'from' => $this->from,
             'line1' => $this->farewellMessage,
-            'line2' => 'Thank you for using ' . self::$appName . '!'
+            'line2' => $this->salutation
         ];
     }
 }
--- app/Notifications/WelcomeNotification.php
+++ app/Notifications/WelcomeNotification.php
@@ -7,6 +7,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 use Illuminate\Support\Facades\Config;
+use JetBrains\PhpStorm\ArrayShape;
 
 class WelcomeNotification extends Notification
 {
@@ -15,7 +16,12 @@ class WelcomeNotification extends Notification
     public User $user;
 
     private static string $appName;
+
+    private readonly string $subject;
     private readonly string $welcomeMessage;
+    private readonly string $salutation;
+    #[ArrayShape(['address' => "string", 'name' => "string"])] private readonly array $from;
+    #[ArrayShape(['text' => "string", 'url' => "string"])] private readonly array $action;
 
     /**
      * Create a new notification instance.
@@ -25,14 +31,18 @@ class WelcomeNotification extends Notification
     public function __construct(User $user)
     {
         $this->user = $user;
+        self::$appName = preg_replace("/^Poke/", POKE_EACUTE, ucfirst(Config::get('app.name')));
 
         $this->setMailProps();
     }
 
     private function setMailProps(): void
     {
-        self::$appName = preg_replace("/^Poke/", POKE_EACUTE, ucfirst(Config::get('app.name')));
+        $this->subject = sprintf("Thank you for joining %s!!", self::$appName);
         $this->welcomeMessage = sprintf("Hello %s, welcome to the world of %s!", $this->user->name, self::$appName);
+        $this->from = ['address' => config('mail.from.address'), 'name' => self::$appName];
+        $this->action = ['text' => 'Check it out!', 'url' => route('roms.index')];
+        $this->salutation = 'Enjoy!';
     }
 
     /**
@@ -55,11 +65,11 @@ class WelcomeNotification extends Notification
     public function toMail(mixed $notifiable): MailMessage
     {
         return (new MailMessage)
-            ->subject(sprintf("Thank you for joining %s!!", self::$appName))
-            ->from(config('mail.from.address'), self::$appName)
+            ->subject($this->subject)
+            ->from(...array_values($this->from))
             ->line($this->welcomeMessage)
-            ->action('Check it out!', route('roms.index'))
-            ->line('Enjoy!');
+            ->action($this->action['text'], $this->action['url'])
+            ->line($this->salutation);
     }
 
     /**
@@ -71,14 +81,11 @@ class WelcomeNotification extends Notification
     public function toArray(mixed $notifiable): array
     {
         return [
-            'subject' => 'Thank you for joining ' . self::$appName . '!!',
-            'from' => [config('mail.from.address'), self::$appName],
+            'subject' => $this->subject,
+            'from' => $this->from,
             'line1' => $this->welcomeMessage,
-            'action' => [
-                'actionText' => 'Check it out!',
-                'actionUrl' => route('roms.index')
-            ],
-            'line2' => 'Enjoy!'
+            'action' => $this->action,
+            'line2' => $this->salutation
         ];
     }
 }
