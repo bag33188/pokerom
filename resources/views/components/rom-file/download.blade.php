f8fae76dd28603370a21cc5edb420dbdfa5b74c0
bag33188 <bglatman@outlook.com>
2022-09-21 18:40:05 -0700

    update code base

--- resources/views/components/rom-file/download.blade.php
+++ resources/views/components/rom-file/download.blade.php
@@ -6,7 +6,7 @@
     @csrf
 
     <x-jet-button type="submit" id="download-romFile-{{ $romFile->_id }}-btn"
-                  class="inline-flex flex-row justify-between gap-2.5">
+                  class="inline-flex flex-row justify-between gap-2">
         <span class="order-0">Download</span>
         <span class="order-1">@include('partials._download-icon')</span>
     </x-jet-button>
