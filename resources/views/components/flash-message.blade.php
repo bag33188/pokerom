d5595247cc16c5490b5d01a4f90824161c3e2424
bag33188 <bglatman@outlook.com>
2022-09-21 17:12:14 -0700

    update code base

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,6 +1,17 @@
+@pushonce('scripts')
+    <script type="text/javascript">
+        function flashData() {
+            return {
+                flashOpen: true
+            }
+        }
+
+        const flashIsSetToOpen = (flashOpen) => flashOpen === true;
+    </script>
+@endpushonce
 @if(Session::has('message'))
     <div {{ $attributes }}
-         x-data="{ flashOpen: true }"
+         x-data="flashData()"
          x-init="setTimeout(() => (flashOpen = false), {{ session()->has('timeout') ? session('timeout') : Js::from(5000) }})"
     >
         @switch(strtolower(session('message-type')))
@@ -8,7 +19,7 @@
                 <div class="w-full flex justify-center">
                     <div
                         class="w-full text-center bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative"
-                        role="alert" type="success" x-show="flashOpen === true">
+                        role="alert" type="success" x-show="flashIsSetToOpen(flashOpen)">
                         <button type="button" @click="flashOpen = false" class="text-red-500 float-right">X</button>
                         <h2 class="font-bold text-xl">Success!</h2>
                         <span class="block sm:inline">{{ session('message') }}</span>
@@ -19,7 +30,7 @@
                 <div class="w-full flex justify-center">
                     <div
                         class="w-full text-center bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative"
-                        role="alert" type="error" x-show="flashOpen === true">
+                        role="alert" type="error" x-show="flashIsSetToOpen(flashOpen)">
                         <button type="button" @click="flashOpen = false" class="text-red-500 float-right">X</button>
                         <h2 class="font-bold text-xl">Error!</h2>
                         <span class="block sm:inline">{{ session('message') }}</span>
@@ -30,7 +41,7 @@
                 <div class="w-full flex justify-center">
                     <div
                         class="w-full text-center bg-blue-100 border border-blue-400 text-blue-600 px-4 py-3 rounded relative"
-                        role="alert" type="info" x-show="flashOpen === true">
+                        role="alert" type="info" x-show="flashIsSetToOpen(flashOpen)">
                         <button type="button" @click="flashOpen = false" class="text-red-500 float-right">X</button>
                         <h2 class="font-bold text-xl">Notice!</h2>
                         <span class="block sm:inline">{{ session('message') }}</span>
--- resources/views/roms/index.blade.php
+++ resources/views/roms/index.blade.php
@@ -39,7 +39,7 @@
                 <span><!--&nbsp;-->ROMs</span>
             </button>
         </div>
-        <x-flash-message/>
+        <x-flash-message class="my-3"/>
         @unless($roms->count() === 0)
             <table class="w-full test-sm text-left text-gray-800 table-auto border-t-2" x-show="open === true">
                 <thead class="bg-gray-50">
