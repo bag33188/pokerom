852c3c5ce418a4fa8e28527e6c6ebaf8005e7bc4
bag33188 <bglatman@outlook.com>
2022-09-22 07:59:43 -0700

    update code base

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta charset="UTF-8"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
         <title>{!! preg_replace("/^[Pp]oke/i", 'Pok&eacute;', config('app.name')) !!}</title>
 
