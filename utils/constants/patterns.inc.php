114afbc8bf031882e6534686b242955207d63bde
bag33188 <bglatman@outlook.com>
2022-09-23 14:03:38 -0700

    update code base

--- utils/constants/patterns.inc.php
+++ utils/constants/patterns.inc.php
@@ -7,7 +7,7 @@
 /** rom filename regular expression */
 const ROM_FILENAME_PATTERN =
     /** @lang RegExp */
-"/^([\w\d\-_]{1,28})\.(gb[ac]?|xci|nds|3ds)$/i";
+"/^([\w\d\-_]{1,28})\.(gb(?:[ac]?)|nds|3ds|xci)$/i";
 /** game name regular expression */
 const GAME_NAME_PATTERN = /** @lang RegExp */
 "/^Pokemon\s.+$/i";
