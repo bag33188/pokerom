7f1420b5f903e22f1587160194765b639078e4de
bag33188 <bglatman@outlook.com>
2022-09-23 09:05:45 -0700

    update code base

--- app/Services/RomFilesConnection.php
+++ app/Services/RomFilesConnection.php
@@ -8,12 +8,14 @@ class RomFilesConnection extends GridFSConnection
 {
     protected string $host = '127.0.0.1';
     protected int $port = 27017;
+
     protected bool $useAuth = true;
     protected string $authDatabase = 'admin';
     protected string $authMechanism = 'SCRAM-SHA-256';
+    protected string $usernameConfigPath = 'database.connections.mongodb.username';
+    protected string $passwordConfigPath = 'database.connections.mongodb.password';
+
     public string $databaseName = 'pokerom_files';
     public string $bucketName = 'rom';
     public int $chunkSize = 0xFF000;
-    protected string $usernameConfigPath = 'database.connections.mongodb.username';
-    protected string $passwordConfigPath = 'database.connections.mongodb.password';
 }
