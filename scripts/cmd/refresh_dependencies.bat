c5617db418bfaa4ed88dcb76b56e91fc4bd60f45
bag33188 <bglatman@outlook.com>
2022-10-04 19:55:12 -0700

    update code base

--- scripts/cmd/refresh_dependencies.bat
+++ scripts/cmd/refresh_dependencies.bat
@@ -8,7 +8,7 @@ CALL :REFRESH_DEPENDENCIES
 EXIT /b %ERRORLEVEL%
 
 :REFRESH_DEPENDENCIES
-    composer install && composer update &&  <nul ^
-    npm install && npm update &&  <nul ^
+    composer install && composer update && <NUL ^
+    npm install && npm update && <NUL ^
     npm run build
 EXIT /b 0
