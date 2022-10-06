d32d9c6514da37e29cb3203dd4fa847c269ea3cc
bag33188 <bglatman@outlook.com>
2022-10-05 21:16:34 -0700

    update code base

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,18 @@
-import { defineConfig } from 'vite';
-import laravel, { refreshPaths } from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel, { refreshPaths } from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/modules/capitalize.js',
-                'resources/js/modules/ready.js',
-                'resources/css/cloak.module.css',
-                'resources/css/scrollbar.module.css',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/modules/capitalize.js",
+                "resources/js/modules/ready.js",
+                "resources/css/cloak.module.css",
+                "resources/css/scrollbar.module.css",
             ],
-            refresh: [
-                ...refreshPaths,
-                'app/Http/Livewire/**',
-            ],
+            refresh: [...refreshPaths, "app/Http/Livewire/**"],
         }),
     ],
 });
