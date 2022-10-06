a0cb87d7ce8fd403e20eb1507e6b56bb221849f4
bag33188 <bglatman@outlook.com>
2022-10-05 22:12:12 -0700

    update code base

--- app/Models/User.php
+++ app/Models/User.php
@@ -77,6 +77,6 @@ class User extends Authenticatable
 
     # public function setPasswordAttribute(string $password): void
     # {
-    #     $this->attributes['password'] = bcrypt($password);
+    #     $this->attributes['password'] = Hash::make($password);
     # }
 }
