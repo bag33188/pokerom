19261fcf03a1ccb47d1e7242a6c9199f5e26b4de
Broccolini <bglatman@callutheran.edu>
2022-10-10 00:02:19 -0700

    update code base

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,6 +1,19 @@
 @props(['heading'])
-<div {{ $attributes->merge(['class' => "p-6 w-full bg-white rounded-lg border border-gray-200 shadow-md"]) }}>
-    <h5 class="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900">{{ $heading }}</h5>
+@php
+    $cardClasses = [
+        'pt-4',
+        'pl-6',
+        'pr-6',
+        'w-full',
+        'bg-white',
+        'rounded-lg',
+        'border',
+        'border-gray-200',
+        'shadow-md'
+    ];
+@endphp
+<div {{ $attributes->merge(['class' => joinCssClasses($cardClasses)]) }}>
+    <h5 class="mb-2 text-2xl text-center font-bold tracking-tight text-gray-900">{!! $heading !!}</h5>
     <div class="w-full bg-white rounded-lg my-2.5">
         {{ $slot }}
     </div>
