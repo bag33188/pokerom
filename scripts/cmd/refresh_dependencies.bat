f106cf7e099e0a2899e386d0a2a6008c78632852
bag33188 <bglatman@outlook.com>
2022-10-13 16:54:38 -0700

    update code base

--- scripts/cmd/refresh_dependencies.bat
+++ scripts/cmd/refresh_dependencies.bat
@@ -1,13 +1,14 @@
 @ECHO OFF
 
-REM |=======================|
-REM | Launch Mailhog Script |
-REM |=======================|
+:: |=======================|
+:: | Launch Mailhog Script |
+:: |=======================|
 
 CALL :REFRESH_DEPENDENCIES
 EXIT /b %ERRORLEVEL%
 
 :REFRESH_DEPENDENCIES
+    REM escape newlines for sequencing commands
     composer install && composer update && <NUL ^
     npm install && npm update && <NUL ^
     npm run build
