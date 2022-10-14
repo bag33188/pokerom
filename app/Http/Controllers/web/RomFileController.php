73447c8d07144de093e5a048dbd188635b0649dd
Broccolini <bglatman@callutheran.edu>
2022-10-14 01:13:31 -0700

    update code base

--- app/Http/Controllers/web/RomFileController.php
+++ app/Http/Controllers/web/RomFileController.php
@@ -33,9 +33,11 @@ class RomFileController extends WebController
     public function index(): Application|Factory|View
     {
         # Gate::authorize('view-romFile');
+
         $romFiles = (auth()->user()->isAdmin())
             ? RomFile::with('rom')->get()
             : RomFile::whereHas('rom')->get();
+
         return view('rom-files.index', [
             'romFiles' => $romFiles,
         ]);
