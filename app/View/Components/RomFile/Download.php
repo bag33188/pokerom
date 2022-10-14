689135b6dca963338484535dd39fb3f33b0d5d88
bag33188 <bglatman@outlook.com>
2022-10-13 21:06:49 -0700

    update code base

--- app/View/Components/RomFile/Download.php
+++ app/View/Components/RomFile/Download.php
@@ -28,6 +28,22 @@ class Download extends Component
      */
     public function render(): View|string|Closure
     {
-        return view('components.rom-file.download');
+        return /** @lang Blade */ <<<'blade'
+            <form {{ $attributes->merge(['class' => 'inline-block']) }}
+                  method="GET"
+                  action="{{ route('rom-files.download', ['romFile' => $romFile]) }}"
+                  name="download-romFile-{{ $romFile->_id }}-form"
+                  enctype="multipart/form-data">
+                @method('GET')
+                @csrf
+
+                <x-jet-button type="submit" id="download-romFile-{{ $romFile->_id }}-btn"
+                              class="flex flex-row justify-between space-x-2 lg:space-x-3">
+                    {{-- `lg` is @media (min-width: 1024px) --}}
+                    <span class="order-0">Download</span>
+                    <span class="order-1">@include('partials._download-icon')</span>
+                </x-jet-button>
+            </form>
+        blade;
     }
 }
