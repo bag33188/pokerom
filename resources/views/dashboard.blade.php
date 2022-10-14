cc7719c2dc56ab87acb7d35c8dcd6a6a56d51195
bag33188 <bglatman@outlook.com>
2022-10-14 11:54:16 -0700

    update code base

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -193,7 +193,6 @@
                                     <br/>
                                     <br/>
                                     Feel free to download them for use with an <i>emulator</i>.
-                                    &nbsp;<b class="text-xl">&rarr;</b>
                                 </p>
                             </div>
                         </div>
