397032b74cff054b19a60f35e304d749b9760465
bag33188 <bglatman@outlook.com>
2022-10-20 16:06:05 -0700

    update code base

--- utils/Classes/AbstractGridChunkModel.php
+++ utils/Classes/AbstractGridChunkModel.php
@@ -8,8 +8,24 @@ use MongoDB\BSON\ObjectId;
 
 abstract class AbstractGridChunkModel extends MongoDbModel
 {
+    /**
+     * Primary key/id of the chunk.
+     * @var string|ObjectId $_id
+     */
     public readonly string|ObjectId $_id;
+    /**
+     * Binary data representation of the chunk.
+     * @var Binary $data
+     */
     public readonly Binary $data;
+    /**
+     * Foreign key/id to the file object.
+     * @var string|ObjectId $files_id
+     */
     public readonly string|ObjectId $files_id;
+    /**
+     * The chunk serial/indexed number of the file.
+     * @var int $n
+     */
     public readonly int $n;
 }
