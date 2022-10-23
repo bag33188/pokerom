df5c288c085aa4aa31ea91c8c806b507025a32b3
bag33188 <bglatman@outlook.com>
2022-10-23 15:10:24 -0700

    update code base

--- utils/functions.php
+++ utils/functions.php
@@ -9,7 +9,7 @@ if (!dir_is_empty($functions_path)) {
     require_once $functions_path . '/str_to_bool.php';
     require_once $functions_path . '/str_capitalize.php';
     require_once $functions_path . '/strip_quotes.php';
-    require_once $functions_path . '/join_css_classes_into_html_string.php';
+    require_once $functions_path . '/join_css_classes_string.php';
 }
 
 //--- END GLOBAL HELPER FUNCTIONS ---//
