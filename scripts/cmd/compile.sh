06cc58f5cacf662287ea6dd2ceeb23dc5786d2a7
Broccolini <50373500+bag33188@users.noreply.github.com>
2022-10-25 07:45:25 -0700

    Update compile.sh

--- scripts/cmd/compile.sh
+++ scripts/cmd/compile.sh
@@ -1,25 +1,6 @@
 #!/usr/bin/bash
 
-# ==============
-# Compile Script
-# ==============
 
-:<< --MULTILINE-COMMENT--
-Permissions (Unix)
-------------------
-add to current permissions
-$ cd scripts/cmd
-$ chmod +x ./compile.sh
-set current permissions
-$ cd scripts/cmd
-$ chmod 755 ./compile.sh
---MULTILINE-COMMENT--
-
-:<< --MULTILINE-COMMENT--
-Make sure you are in Bash.
-$ echo $0
-/usr/bin/bash # bash
---MULTILINE-COMMENT--
 
 compile() {
     bold=$(tput bold)
@@ -27,23 +8,17 @@ compile() {
     target_PWD=$(readlink -f .)
     current_folder="${target_PWD##*/}"
 
-    if [[ "$current_folder" = "scripts" ]]; then
-        cd ..
-    fi
-    if [[ "$current_folder" = "cmd" ]]; then
-        cd ../..
+    if [[ "$current_folder" != "pokerom" ]]; then
+        cd $pokerom
     fi
 
     composer install && composer update
     npm install && npm update
     npm run build
 
-    node scripts/helpers/seeds.test.js
-
     git status
-    git add . && git commit -m "update code base" && git push
 
-    # printf "\n${bold}%s${normal}\n" "Finished!"
+    git add . && git commit -m "update code base" && git push
 
     echo -e "\n${bold}Finished!${normal}"
     date +"%r"
